605fca0ecda25aa85771e55925b6ab7a3c285175
abc <1317517250@qq.com>
2020-11-04 14:20:25 +0800

    11

--- XunLei/groupExercise/showViewAgain.swift
+++ XunLei/groupExercise/showViewAgain.swift
@@ -15,7 +15,9 @@ class vmModelShow: NSObject {
 //   init(textPusblish:Observable<String>) {
 //   showEnd = textPusblish.map{"e"+$0+"e"}.share(replay:1)
 //}
-    
+    //回滚测试1
+    //回滚测试2
+    //回滚测试3
     let pageshow:Driver<String>
    //let twoPageShow:Driver<String>//声明twoPageShow:Driver<String>需要去初始化对象
    init(oneShow:Driver<String>) {
